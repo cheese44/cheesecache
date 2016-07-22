3bb4006308006e65c0d0bb32549908bc8a0996ab
Devon <norosoboshi@gmail.com>
2016-07-23 00:29:25 +0200

    added factory for ease of use

---
+++ src/app/CacheProvider.php
@@ -0,0 +1,25 @@
+<?php
+
+  namespace cheeseCache\app;
+
+  use cheeseCache\interfaces as cheeseInterfaces;
+  use cheeseCache\exceptions as cheeseExceptions;
+
+  /**
+   * @author cheese44
+   */
+  class CacheProvider {
+
+    private static $cache;
+    
+    public static function getCache() {
+      if(self::$cache instanceof cheeseInterfaces\ICache):
+        return self::$cache;
+      endif;
+      
+      self::$cache = new Cache();
+      
+      return self::$cache;
+    }
+    
+  }
